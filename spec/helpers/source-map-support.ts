b4707d908efbcbb8107f8376f43f4bf86ecbe9ae
pastelmind <keepyourhonor@gmail.com>
2021-01-01 19:06:24 +0900

    test: Set up Jasmine config
    - Add Jasmine config (generated via `npx jasmine init`)
    - `npm test` runs Jasmine
    - Also source-map-support.ts, which loads source-map-support/register
      to enable friendly stack traces

---
+++ spec/helpers/source-map-support.ts
@@ -0,0 +1,6 @@
+/**
+ * @file Adds source map support for error stack traces emitted while running
+ * Jasmine.
+ */
+// eslint-disable-next-line node/no-unpublished-import
+import 'source-map-support/register';
